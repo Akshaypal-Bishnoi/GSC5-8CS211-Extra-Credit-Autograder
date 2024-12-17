8e2d5b061644ae5f5b1d753cd73776aef5afa832
Akshaypal-Bishnoi <akshaypal187829@gmail.com>
2024-12-18 00:35:27 +0530

    Create Fourth.c
    .h file has created

---
+++ Fourth.c
@@ -0,0 +1,10 @@
+#ifndef BINARY_CONVERTER_H
+#define BINARY_CONVERTER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void printBinary(unsigned int num, int bits);
+void processFile(const char *filename);
+
+#endif // BINARY_CONVERTER_H
